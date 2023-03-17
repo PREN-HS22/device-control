6ac9b337dbc17db3705aa45dd58ae95f4225fd45
Sheggy <8018511+Sheggies@users.noreply.github.com>
2023-03-17 20:16:36 +0100

    Use SlidingRange to record weight measurement data

--- LoadCell/LoadCell.hpp
+++ LoadCell/LoadCell.hpp
@@ -2,6 +2,7 @@
 #include "Emergency.hpp"
 #include "Reportable.hpp"
 #include "HX711_ADC.h"
+#include "SlidingRange.hpp"
 
 namespace PREN::Component
 {
@@ -11,6 +12,7 @@ namespace PREN::Component
     private:
         HX711_ADC device;
         Report status;
+        SlidingRange<D * S> data;
 
     public:
         LoadCell(unsigned int dout, unsigned int sck);
