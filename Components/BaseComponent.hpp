ea22d1d10b81b4539017be1021f0293af2d226d6
Sheggy <8018511+Sheggies@users.noreply.github.com>
2023-04-03 14:21:21 +0200

    Bring types from PREN namespace into scope

--- Components/BaseComponent.hpp
+++ Components/BaseComponent.hpp
@@ -7,6 +7,8 @@ namespace PREN
     class Controller;
 }
 
+using PREN::Emergency, PREN::Reportable, PREN::Controller, PREN::Report;
+
 namespace PREN::Components
 {
     class BaseComponent : public Emergency, public Reportable
