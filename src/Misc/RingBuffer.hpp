de1bd8ec852fe21548262a057559afd224d2e957
Sheggy <8018511+Sheggies@users.noreply.github.com>
2023-04-05 12:10:44 +0200

    Fix several RingBuffer bugs

--- src/Misc/RingBuffer.hpp
+++ src/Misc/RingBuffer.hpp
@@ -11,7 +11,7 @@ namespace PREN
         bool empty;
         unsigned int head, tail;
         std::array<T, N> buffer;
-        friend class SlidingRange;
+        template <std::size_t M> friend class SlidingRange;
 
     public:
         RingBuffer() : empty(true), head(0), tail(0) {}
@@ -19,21 +19,18 @@ namespace PREN
 
         void Push(T obj)
         {
-            // Move head one slot ahead if buffer is not empty
             this->head = this->empty ? this->head : this->head + 1;
             // Wrap around head if it exceeds the buffer size
             this->head = this->head < this->GetCapacity() ? this->head : 0;
 
+            // Move tail ahead if head approaches
+            this->tail = this->empty
+                || this->head != this->tail
+                ? this->tail : this->tail + 1;
+            // Wrap around tail if it exceeds the buffer size
+            this->tail = this->tail < this->GetCapacity() ? this->tail : 0;
             this->buffer[this->head] = obj;
             this->empty = false;
-
-            // Override and move tail one slot ahead if buffer is not empty
-            if (this->tail == this->head && !this->empty)
-            {
-                this->tail++;
-                // Wrap around tail if it exceeds the buffer size
-                this->tail = this->tail < this->GetCapacity() ? this->tail : 0;
-            }
         }
 
         void Clear()
@@ -45,28 +42,44 @@ namespace PREN
 
         T Pop()
         {
-            if (!this->IsEmpty())
+            if (this->empty)
             {
                 return;
             }
 
             T next = this->buffer[this->tail];
-            this->tail++;
+            this->empty = this->tail == this->head;
+            this->tail = this->empty ? this->tail : this->tail + 1;
             // Wrap around tail if it exceeds the buffer size
             this->tail = this->tail < this->GetCapacity() ? this->tail : 0;
-            this->empty = this->tail == this->head;
             return next;
         }
 
-        T PeekHead() { return this->buffer[this->head]; }
-        T PeekTail() { return this->buffer[this->tail]; }
-        std::size_t GetCapacity() { return this->buffer->max_size; }
+        T PeekHead()
+        {
+            if (this->empty)
+            {
+                return;
+            }
+            return this->buffer[this->head];
+        }
+
+        T PeekTail()
+        {
+            if (this->empty)
+            {
+                return;
+            }
+            return this->buffer[this->tail];
+        }
+
+        std::size_t GetCapacity() const noexcept { return this->buffer.max_size(); }
         bool IsEmpty() { return this->empty; }
 
         // Provide constant iterator for range iteration
-        typename std::array<T, N>::const_iterator begin() const { return this->buffer->begin(); }
-        typename std::array<T, N>::const_iterator cbegin() const { return this->buffer->cbegin(); };
-        typename std::array<T, N>::const_iterator end() const { return this->buffer->end(); }
-        typename std::array<T, N>::const_iterator cend() const { return this->buffer->cend(); }
+        typename std::array<T, N>::const_iterator begin() const { return this->buffer.begin(); }
+        typename std::array<T, N>::const_iterator cbegin() const { return this->buffer.cbegin(); };
+        typename std::array<T, N>::const_iterator end() const { return this->buffer.end(); }
+        typename std::array<T, N>::const_iterator cend() const { return this->buffer.cend(); }
     };
 }
