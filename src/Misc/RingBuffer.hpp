8c2af09f4590c130993f9d43c4123f446c0ab5b7
Sheggy <8018511+Sheggies@users.noreply.github.com>
2023-04-10 20:40:01 +0200

    Refactor and fix iterator bugs in RingBuffer

--- src/Misc/RingBuffer.hpp
+++ src/Misc/RingBuffer.hpp
@@ -8,112 +8,130 @@ namespace PREN
     class RingBuffer
     {
     private:
-        bool empty;
+        bool overflowed;
         unsigned int head, tail;
         std::array<T, N> buffer;
         template <std::size_t M>
         friend class SlidingRange;
+
+        void advanceHead()
+        {
+            ++this->head;
+            this->head = this->head < N ? this->head : 0;
+        }
+
+        void advanceTail()
+        {
+            ++this->tail;
+            this->tail = this->tail < N ? this->tail : 0;
+        }
+
         class Iterator
         {
         private:
-            std::array<T, N> *range;
-            unsigned int next, end;
+            RingBuffer<T, N> *rb;
+            unsigned int next;
+            bool overflowed;
 
         public:
-            Iterator(std::array<T, N> *range, unsigned int next, unsigned int end) : range(range), next(next), end(end) {}
+            Iterator(RingBuffer *rb) : rb(rb), next(rb->tail), overflowed(rb->overflowed) {}
             ~Iterator() {}
 
             Iterator &operator++()
             {
                 this->next++;
+                // Wrap around tail if it exceeds the buffer size
                 this->next = this->next < N ? this->next : 0;
+
                 return *this;
             }
 
-            T const &operator*() const { return this->range->at(next); }
-            T &operator*() { return this->range->at(next); }
-
-            bool operator!=(Iterator const &rhs) const
+            Iterator &operator+(unsigned int i)
             {
-                if (rhs.next == rhs.end)
-                {
-                    return false;
-                }
-
-                if (this->next == rhs.end)
-                {
-                    ++rhs;
-                }
-
-                return true;
+                this->next = (this->next + i) % N;
+                this->overflowed = false;
+                return *this;
             }
+
+            bool operator!=(std::size_t i)
+            {
+                if (this->overflowed && this->next == i)
+                {
+                    this->overflowed = false;
+                    return true;
+                }
+
+                return this->next != i;
+            }
+
+            T const &operator*() const { return this->rb->buffer.at(this->next); }
+            T &operator*() { return this->rb->buffer.at(this->next); }
         };
 
     public:
-        RingBuffer() : empty(true), head(0), tail(0) {}
+        RingBuffer() : overflowed(false), head(0), tail(0) {}
         ~RingBuffer() {}
 
         void Push(T obj)
         {
-            this->head = this->empty ? this->head : this->head + 1;
-            // Wrap around head if it exceeds the buffer size
-            this->head = this->head < this->GetCapacity() ? this->head : 0;
+            if (this->overflowed)
+            {
+                this->advanceTail();
+            }
 
-            // Move tail ahead if head approaches
-            this->tail = this->empty || this->head != this->tail
-                             ? this->tail
-                             : this->tail + 1;
-            // Wrap around tail if it exceeds the buffer size
-            this->tail = this->tail < this->GetCapacity() ? this->tail : 0;
             this->buffer[this->head] = obj;
-            this->empty = false;
-        }
-
-        void Clear()
-        {
-            this->empty = true;
-            this->head = 0;
-            this->tail = 0;
+            this->advanceHead();
+            this->overflowed = this->tail == this->head;
         }
 
         T Pop()
         {
-            if (this->empty)
+            if (this->IsEmpty())
             {
                 return;
             }
 
             T next = this->buffer[this->tail];
-            this->empty = this->tail == this->head;
-            this->tail = this->empty ? this->tail : this->tail + 1;
-            // Wrap around tail if it exceeds the buffer size
-            this->tail = this->tail < this->GetCapacity() ? this->tail : 0;
+            this->advanceTail();
+            this->overflowed = false;
             return next;
         }
 
         T PeekHead()
         {
-            if (this->empty)
+            if (this->IsEmpty())
             {
                 return;
             }
-            return this->buffer[this->head];
+
+            auto prevHead = (this->head - 1 + N) % N;
+            return this->buffer[prevHead];
         }
 
         T PeekTail()
         {
-            if (this->empty)
+            if (this->tail == this->head && !this->overflowed)
             {
                 return;
             }
             return this->buffer[this->tail];
         }
 
-        std::size_t GetCapacity() const noexcept { return this->buffer.max_size(); }
-        bool IsEmpty() { return this->empty; }
+        void Clear()
+        {
+            this->overflowed = false;
+            this->head = 0;
+            this->tail = 0;
+        }
+
+        std::size_t GetCapacity() const noexcept { return N; }
+        bool IsEmpty() { return this->tail == this->head && !this->overflowed; }
 
         // Provide constant iterator for range iteration
-        Iterator begin() const { return Iterator(&this->buffer, this->tail, this->head); }
-        Iterator end() const { return Iterator(&this->buffer, this->head - 1, this->head); }
+        Iterator begin() const
+        {
+            return Iterator(this);
+        }
+        std::size_t end() const { return this->head; }
     };
 }
