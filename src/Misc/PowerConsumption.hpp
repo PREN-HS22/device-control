b030bc7eb2eddb3826b3f0d5a3d9e61497caabbc
Sheggy <8018511+Sheggies@users.noreply.github.com>
2023-05-11 10:15:31 +0200

    Refactor PowerConsumption.hpp

--- src/Misc/PowerConsumption.hpp
+++ src/Misc/PowerConsumption.hpp
@@ -7,6 +7,7 @@ namespace CleaningDevice
     class PowerConsumption
     {
     private:
+        using const_iterator = typename std::array<float, N>::const_iterator;
         RingBuffer<float, N> data;
         float totalWatts;
 
@@ -17,6 +18,7 @@ namespace CleaningDevice
         void AddWattsMeasurement(float v)
         {
             this->data.Push(v);
+            // TODO: Multiply `v` with `dt` to correct for the time delta
             this->totalWatts += v;
         }
 
@@ -26,9 +28,9 @@ namespace CleaningDevice
         float GetTotalWatts() { return this->totalWatts; }
 
         // Implement iterator
-        typename std::array<float, N>::const_iterator begin() const { return this->data.begin(); }
-        typename std::array<float, N>::const_iterator cbegin() const { return this->data.cbegin(); }
-        typename std::array<float, N>::const_iterator end() const { return this->data.end(); }
-        typename std::array<float, N>::const_iterator cend() const { return this->data.cend(); }
+        const_iterator begin() const { return this->data.begin(); }
+        const_iterator cbegin() const { return this->data.cbegin(); }
+        const_iterator end() const { return this->data.end(); }
+        const_iterator cend() const { return this->data.cend(); }
     };
 }
