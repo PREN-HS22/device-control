ef1dd8c72810d9751a86c8dc7821245a6b09ebea
Sheggy <8018511+Sheggies@users.noreply.github.com>
2023-05-25 13:13:28 +0200

    Implement DC motor controls

--- src/Components/DcMotor/DcMotor.cpp
+++ src/Components/DcMotor/DcMotor.cpp
@@ -2,43 +2,58 @@
 
 namespace CleaningDevice::Components
 {
-    DcMotor::DcMotor(Controller &c, State<DcMotor> *start)
-        : AbstractComponent(c, start)
+    DcMotor::DcMotor(Controller &c, State<DcMotor> *start, DcMotorCfg cfg)
+        : AbstractComponent(c, start),
+          config(cfg),
+          motor(cfg.Enable, cfg.In1, cfg.In2)
     {
+        this->motor.setSpeed(cfg.Pwm);
     }
 
     DcMotor::~DcMotor()
     {
     }
 
-    void DcMotor::Rotate(float angularVel, float torque)
+    void DcMotor::Rotate(L298N::Direction dir, uint16_t pwmDutyCycle)
     {
-        this->angularVel = angularVel;
-        this->torque = torque;
+        this->motor.setSpeed(pwmDutyCycle);
+        this->motor.runFor(0, dir);
     }
 
-    void DcMotor::Rotate(float angularVel, float torque, float duration)
+    void DcMotor::Rotate(L298N::Direction dir, uint16_t pwmDutyCycle, float duration)
     {
-        this->angularVel = angularVel;
-        this->torque = torque;
+        this->motor.setSpeed(pwmDutyCycle);
+        this->motor.runFor(duration, dir);
     }
 
     void DcMotor::Stop()
     {
-        this->angularVel = 0.f;
-        this->torque = 0.f;
+        this->motor.stop();
     }
 
     void DcMotor::RaiseEmergency()
     {
-        // Stop device
+        this->motor.stop();
     }
 
     Report &DcMotor::GetReport()
     {
+        std::string dir;
+        switch (this->motor.getDirection())
+        {
+        case L298N::FORWARD:
+            dir = "Forward";
+            break;
+        case L298N::BACKWARD:
+            dir = "Backward";
+            break;
+        case L298N::STOP:
+            dir = "Stopped";
+            break;
+        }
         this->report["Status"] = this->GetState()->GetName();
-        this->report["AngularVelocity"] = this->angularVel;
-        this->report["Torque"] = this->torque;
+        this->report["Duty cycle"] = this->motor.getSpeed();
+        this->report["Direction"] = dir.c_str();
 
         return this->report;
     }
--- src/Components/DcMotor/DcMotor.hpp
+++ src/Components/DcMotor/DcMotor.hpp
@@ -1,19 +1,34 @@
 #pragma once
 #include "../AbstractComponent.hpp"
+#include <L298N.h>
 
 namespace CleaningDevice::Components
 {
+    class DcMotorCfg
+    {
+    public:
+        const std::uint8_t Enable, In1, In2;
+        const std::uint16_t Pwm;
+
+        DcMotorCfg(std::uint8_t enable, std::uint8_t in1, std::uint8_t in2, std::uint16_t pwm = 0)
+            : Enable(enable),
+              In1(in1),
+              In2(in2),
+              Pwm(pwm) {}
+    };
+
     class DcMotor : public AbstractComponent<DcMotor>
     {
     private:
-        float angularVel, torque;
+        DcMotorCfg config;
+        L298N motor;
 
     public:
-        DcMotor(Controller &c, State<DcMotor> *start);
+        DcMotor(Controller &c, State<DcMotor> *start, DcMotorCfg config);
         ~DcMotor();
 
-        void Rotate(float angularVel, float torque);
-        void Rotate(float angularVel, float torque, float duration);
+        void Rotate(L298N::Direction dir, uint16_t pwmDutyCycle);
+        void Rotate(L298N::Direction dir, uint16_t pwmDutyCycle, float duration);
         void Stop();
         void RaiseEmergency();
         Report &GetReport();
