c7d03fca53f48e04a0168f6fe7cbb10f3fd20c80
Sheggy <8018511+Sheggies@users.noreply.github.com>
2023-06-06 14:20:34 +0200

    Simplify power consumption implementation

--- src/Misc/PowerConsumption.hpp
+++ src/Misc/PowerConsumption.hpp
@@ -1,36 +1,28 @@
 #pragma once
-#include "RingBuffer.hpp"
 
 namespace CleaningDevice
 {
-    template <std::size_t N>
     class PowerConsumption
     {
     private:
-        using const_iterator = typename std::array<float, N>::const_iterator;
-        RingBuffer<float, N> data;
-        float totalWatts;
+        float current;
+        float total;
+        float deltaTime;
 
     public:
-        PowerConsumption() : totalWatts(0) {}
+        PowerConsumption() : total(0) {}
         ~PowerConsumption() {}
 
-        void AddWattsMeasurement(float v)
+        float GetDeltaTime() { return this->deltaTime; }
+        void SetDeltaTime(float dt) { this->deltaTime = dt; }
+
+        float GetCurrent() { return this->current; }
+        void SetCurrent(float v)
         {
-            this->data.Push(v);
-            // TODO: Multiply `v` with `dt` to correct for the time delta
-            this->totalWatts += v;
+            this->current = v;
+            this->total += v * this->deltaTime;
         }
 
-        void Clear() { this->data.Clear(); }
-        std::size_t GetCapacity() { return this->data.GetCapacity(); }
-        float GetCurrentWatts() { return this->data.PeekHead(); }
-        float GetTotalWatts() { return this->totalWatts; }
-
-        // Implement iterator
-        const_iterator begin() const { return this->data.begin(); }
-        const_iterator cbegin() const { return this->data.cbegin(); }
-        const_iterator end() const { return this->data.end(); }
-        const_iterator cend() const { return this->data.cend(); }
+        float GetTotal() { return this->total; }
     };
 }
