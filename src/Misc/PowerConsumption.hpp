984d52cc20066c82c9b37105db64ace0379832ed
Sheggy <8018511+Sheggies@users.noreply.github.com>
2023-07-02 21:50:55 +0200

    Replace power consumption task with timer

--- src/Misc/PowerConsumption.hpp
+++ src/Misc/PowerConsumption.hpp
@@ -17,67 +17,53 @@ namespace CleaningDevice
     private:
         Report report;
         MeasurementSample sample;
-        TaskHandle_t task;
+        TimerHandle_t timer;
         portMUX_TYPE spinLock = portMUX_INITIALIZER_UNLOCKED;
         TwoWire i2c;
         Adafruit_INA219 ina;
         float sampleRate, totalPower;
 
     private:
-        static void Run(void *pvParams)
+        static void TimerCallback(TimerHandle_t timer)
         {
-            auto self = static_cast<PowerConsumption *>(pvParams);
-            TickType_t lastExecutedTick = xTaskGetTickCount();
-
-            taskENTER_CRITICAL(&(self->spinLock));
-            // ----
-            auto i2c = self->i2c;
+            auto self = static_cast<PowerConsumption *>(pvTimerGetTimerID(timer));
             auto ina = self->ina;
             auto sample = self->sample;
             auto delta = 1.f / self->sampleRate;
 
-            int i = 0;
-            while (!ina.begin(&i2c))
-            {
-                if (i++ > 9)
-                {
-                    vTaskDelete(nullptr);
-                }
-
-                vTaskDelay(pdMS_TO_TICKS(100));
-            }
+            taskENTER_CRITICAL(&(self->spinLock));
+            // ----
+            sample.ShuntVoltage = ina.getShuntVoltage_mV() / 1000.f;
+            sample.BusVoltage = ina.getBusVoltage_V();
+            sample.Current = ina.getCurrent_mA() / 1000.f;
+            sample.Power = ina.getPower_mW() / 1000.f;
+            sample.LoadVoltage = sample.BusVoltage + (sample.ShuntVoltage);
+            self->totalPower += sample.Power * delta;
             // ----
             taskEXIT_CRITICAL(&(self->spinLock));
-
-            for (;;)
-            {
-                taskENTER_CRITICAL(&(self->spinLock));
-                // ----
-                sample.ShuntVoltage = ina.getShuntVoltage_mV() / 1000.f;
-                sample.BusVoltage = ina.getBusVoltage_V();
-                sample.Current = ina.getCurrent_mA() / 1000.f;
-                sample.Power = ina.getPower_mW() / 1000.f;
-                sample.LoadVoltage = sample.BusVoltage + (sample.ShuntVoltage);
-                self->totalPower += sample.Power * delta;
-                // ----
-                taskEXIT_CRITICAL(&(self->spinLock));
-
-                xTaskDelayUntil(&lastExecutedTick, pdMS_TO_TICKS(1000.f * delta));
-            }
         }
 
     public:
         PowerConsumption(float rate, std::uint8_t address)
-            : sampleRate(rate),
+            : sampleRate(rate < 1.f ? 1.f : rate),
               totalPower(0),
               i2c(0),
               ina(address)
         {
             this->i2c.setPins(13, 4);
-            xTaskCreatePinnedToCore(PowerConsumption::Run, "PowerConsumption::Run", CONFIG_ARDUINO_LOOP_STACK_SIZE, this, tskIDLE_PRIORITY, &(this->task), 0);
+            this->timer = xTimerCreate("PowerConsumption::TimerCallback", pdMS_TO_TICKS((TickType_t)(1000.f / rate)), pdFALSE, this, PowerConsumption::TimerCallback);
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (ina.begin())
+                {
+                    break;
+                }
+                delay(50);
+            }
         }
 
-        ~PowerConsumption() { vTaskDelete(this->task); }
+        ~PowerConsumption() { xTimerDelete(this->timer, pdMS_TO_TICKS(5)); }
 
         float GetSampleRate() { return this->sampleRate; }
         void SetSampleRate(float rate) { this->sampleRate = rate; }
