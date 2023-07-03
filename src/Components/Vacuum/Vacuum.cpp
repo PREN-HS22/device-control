31fe22797b37c311e5c5284059d2d17cb274222c
Sheggy <8018511+Sheggies@users.noreply.github.com>
2023-07-03 15:44:49 +0200

    Refactor vacuum class

--- src/Components/Vacuum/Vacuum.cpp
+++ src/Components/Vacuum/Vacuum.cpp
@@ -7,7 +7,6 @@ namespace CleaningDevice::Components
           speedFraction(0.f),
           motor()
     {
-        this->motor.attach(18, Servo::CHANNEL_NOT_ATTACHED, 0, 100);
     }
 
     Vacuum::~Vacuum()
@@ -18,19 +17,44 @@ namespace CleaningDevice::Components
 
     void Vacuum::Start()
     {
-        this->motor.write(this->speedFraction * 100.f);
+        if (this->motor.attached())
+        {
+            return;
+        }
+
+        // Min pulse width: 900us, max pulse width 2140us
+        this->motor.attach(18, Servo::CHANNEL_NOT_ATTACHED, 0, 1000, 900, 2140);
+        this->motor.write(0);
     }
 
     void Vacuum::Stop()
     {
-        this->motor.write(0.f);
+        if (!this->motor.attached())
+        {
+            return;
+        }
+
+        this->motor.write(0);
+        this->motor.detach();
     }
 
     void Vacuum::SetTargetSpeed(float fraction)
     {
+        if (!this->motor.attached())
+        {
+            this->speedFraction = 0.f;
+            return;
+        }
+
+        // Cut-off % -> 17.5
+        // Idle % -> 18.0
+        // Min % cig -> 22.0
+        // Min % cap -> 22.0
+        // Min % metal cap -> 26.0
+        // Max %. loudness -> 28.0
         fraction = std::clamp(fraction, 0.f, 1.f);
         this->speedFraction = fraction;
-        motor.write(fraction * 100.f);
+        motor.write((int)(fraction * 1000.f));
     }
 
     float Vacuum::GetTargetSpeed()
@@ -38,7 +62,8 @@ namespace CleaningDevice::Components
         return this->speedFraction;
     }
 
-    bool Vacuum::IsReady() {
+    bool Vacuum::IsReady()
+    {
         return this->motor.attached();
     }
 
